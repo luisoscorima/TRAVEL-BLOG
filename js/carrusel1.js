f5103ae25b1d01b36f643bdf83f7038a7613ae05
Babi2098 <priscilaward.20@gmail.com>
2025-02-23 20:57:28 -0500

    agregue carrusel1

---
+++ js/carrusel1.js
@@ -0,0 +1,34 @@
+const swiper = new Swiper(".swiper", {
+    slidesPerView: 5,
+    spaceBetween: 0,
+    centeredSlides: true,
+    loop: true,
+    simulateTouch: 'ontouchstart' in window || navigator.maxTouchPoints > 0,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    }
+  })
+  
+  const calculateHeight = () => {
+    const swiperSlideElements = Array.from(document.querySelectorAll('.swiper .swiper-slide'))
+    if (!swiperSlideElements.length) return
+    const width = swiperSlideElements[0].getBoundingClientRect().width
+    const height = Math.round(width / (16 / 9))
+    swiperSlideElements.map(element => element.style.height = `${height}px`)
+  }
+  
+  document.addEventListener("DOMContentLoaded", calculateHeight)
+  addEventListener('resize', calculateHeight)
+
+//Funcionalidad botones
+document.addEventListener("DOMContentLoaded", () => {
+    $('.btn').on('click', function() {
+      var $this = $(this);
+      $this.button('loading');
+      setTimeout(function() {
+        $this.button('reset');
+      }, 8000);
+    });
+  });
+
